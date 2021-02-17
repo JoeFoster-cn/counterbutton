d40250642bc1e46d240e5a444de18cbbbcdaa89e
Joe Foster <jfoster29492@gmail.com>
2021-02-17 12:49:22 +0000

    completed first part of activity

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    number : 0
-  }
-  increaseNum = (event) => {
-    // let number = parseInt(event.target.number)
-    this.setState({ number: this.state.number + 1})
-  };
-
-  render() {
+class App extends Component { //class component
+  render() { //renders the counter component returned in the ui
     return (
-        <div className="App">
-          <button onClick={this.increaseNum}></button>
-        </div>
-      );
-  } 
+      <div className="App">
+        <Counter/>
+      </div>
+    );
+  }
+}
+
+class Counter extends Component { //counter component 
+  state = {
+    count: 0 //count variable set as 0
+  };
+  clickCount = () => { //clicking function
+    this.setState(({ count }) => ({ //set state to count + 1 each time clickCount is called
+      count: count + 1
+    }))
+  };
+  render() { //render this for the counter component
+    return <button onClick ={this.clickCount}>{this.state.count}</button>;
+  } //return button onClick to be clickCount funtion and returns the state containing count which is being incremented by clickCount
 }
 
 export default App;
